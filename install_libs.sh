29d05bacfad2ad76c1ebea42ad75c833e30ec9de
GiuseppeSatta <121334572+GiuseppeSatta@users.noreply.github.com>
2025-11-21 15:12:14 +0100

    Update install_libs.sh

--- install_libs.sh
+++ install_libs.sh
@@ -1,39 +1,38 @@
-et -e
+#!/bin/bash
+set -e
 
 echo "Updating package list..."
 sudo apt update
 
 echo "Installing required packages for building kernel modules and OpenGL ES 2.0..."
 sudo apt install -y \
-	build-essential \
-	dkms \
-	linux-headers-$(uname -r) \
-	gcc \
-	make \
-	perl \
-	git \
-	dtc \
-	bc \
-	bison \
-	flex \
-	libelf-dev \
-	libssl-dev \
-	libncurses-dev \
-	wget \
-    curl \
-	vim \
-	libfdt-dev \
-	fpga-manager-xlnx \
-	libncurses5-dev \
-	libtinfo5 \
-	xterm \
-	autoconf \
-	libtool \
-	texinfo \
-	gcc-multilib \
-	libgles2-mesa-dev \
-	libegl1-mesa-dev \
-	imagemagick
-										
+    build-essential \
+    dkms \
+    linux-headers-$(uname -r) \
+    gcc \
+    make \
+    perl \
+    git \
+    device-tree-compiler \
+    bc \
+    bison \
+    flex \
+    libelf-dev \
+    libssl-dev \
+    libncurses-dev \
+    wget \
+        curl \
+    vim \
+    libfdt-dev \
+    fpga-manager-xlnx \
+    libncurses5-dev \
+    libtinfo5 \
+    xterm \
+    autoconf \
+    libtool \
+    texinfo \
+    libgles2-mesa-dev \
+    libegl1-mesa-dev \
+    imagemagick
+                                        
 echo "All necessary packages have been installed."
-
